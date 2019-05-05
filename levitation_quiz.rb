77d92f790320927b659e5a5940b4bdec5c0957d8
Rori Hinds <r_ori@hotmail.co.uk>
2019-05-05 01:21:38 +0000

    Automatically backed up by Learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,7 +1,7 @@
 
 def levitation_quiz
 	loop do 
-        puts "What's up, Doc?"
+        puts "What is the spell that enacts levitation?"
         answer = gets.chomp
         break if answer == "STOP"
     end
