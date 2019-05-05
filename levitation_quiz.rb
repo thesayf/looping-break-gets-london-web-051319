b3c85b9d45f8aa9c2aec163a21b815f54e9cd5c5
Rori Hinds <r_ori@hotmail.co.uk>
2019-05-05 01:21:27 +0000

    Automatically backed up by Learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,6 +1,11 @@
 
 def levitation_quiz
-	#your code here
+	loop do 
+        puts "What's up, Doc?"
+        answer = gets.chomp
+        break if answer == "STOP"
+    end
+    puts "Okay, okay, jeez. I'll stop. Sorry."
 end
 
 
